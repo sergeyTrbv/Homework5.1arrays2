015e0506483929371186107affd395cffbdb513a
1 <sergey.trbv@yandex.ru>
2022-09-27 02:02:10 +0400

    Готовое дз

--- src/Main.java
+++ src/Main.java
@@ -63,6 +63,7 @@ public class Main {
             System.out.print(arr[i] + " ");
         }
 
+
         int summ = 0;
         for (int i = 0; i < arr.length; i++) {
             summ = summ + arr[i];
